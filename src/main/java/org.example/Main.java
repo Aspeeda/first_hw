efb3a32ca2f4235dbfcd618fb4ef715637cb400e
DESKTOP-T1P4KAC\User <aspeeda@yandex.ru>
2022-11-13 10:14:20 +0500

    first commit

--- src/main/java/org.example/Main.java
+++ src/main/java/org.example/Main.java
@@ -4,6 +4,6 @@ package src.main.java.org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!!!!!!!!!!!!");
-        System.out.println("My name is [Censored]");
+        System.out.println("Who am I?");
     }
 }
